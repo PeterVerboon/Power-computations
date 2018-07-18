64a8d8964abf40b9702f38da8e1ee4118360303f
PeterVerboon <Peter.Verboon@ou.nl>
2018-07-18 17:47:20 +0200

    diverse uitbreidingen

--- power moderatie.R
+++ power moderatie.R
@@ -5,44 +5,99 @@ require(userfriendlyscience)
 require(MASS)
 require(lm.beta)
 
-N=500
-rho1 <- .5
-rep <- 500
-e <- sqrt(3)
-
-out <- matrix(0,nrow=rep,ncol=8)
-varxz <- rep(0,rep)
-vary <- rep(0,rep)
-
-for (i in 1:rep) {
-  
-  a <- simDataSet(N, varNames = c("x","z"),  means=c(0,0), sds = c(1, 1), specifiedCorrelations = list(c('x','z',rho1)), ranges = NULL, silent=TRUE, seed = NULL)
-  x <- a$x; z <- a$z
-  xz <- x*z
-  varxz[i] <- var(xz)
-
-  error <-  rnorm(N,0,sqrt(e))   # the amount of error determines the ES
-
-   y <- sqrt(.5)*x + sqrt(.3)*z + sqrt(.2)*xz + error
-
-   vary[i] <- var(y)
-
-   res <- lm( y ~ x + z + xz) 
-   res <- lm.beta(res)
-   sig <- summary(res)$coefficients[4,5] < .05
-   rsq <- summary(res)$r.squared
+options(digits = 3)
  
-  out[i,] <- c(rsq, res$coef[-1], res$standardized.coefficients[-1], sig)
-}
+
+simPower.moderation <- function(samSize = c(50,100,150,200,250,300), 
+                                errlevel = c(1,3,9), 
+                                rholevel = c(0,.3,.5,.8),
+                                betas = c(.5, .3, .2),
+                                rep = 1000) 
+      {   
+    numrow <- length(errlevel)*length(rholevel)*length(samSize)
+    result <- as.data.frame(matrix(data=0, nrow = numrow, ncol= 8))
+    colnames(result) <- c("N","e","rho", "rsq", "b1","b2","b3","power")
+    result[,"e"] <- rep(sort(rep(errlevel,length(rholevel))),length(samSize))
+    result[,"rho"] <- rep(rholevel,length(errlevel)*length(samSize))
+    result[,"N"] <- sort(rep(samSize,length(errlevel)*length(rholevel)))
+    
+    b1 <- betas[1]
+    b2 <- betas[2]
+    b3 <- betas[3]
+    
+     for (N in samSize)
+     {
+       for (e in errlevel) 
+         { 
+         for (rho in rholevel)
+           {
+           sigma = matrix(c(1,rho,rho,1), nrow=2, ncol=2)
+           
+           out <- matrix(0,nrow=rep,ncol=5)
+           
+           for (i in 1:rep) 
+             {
+  
+      a <- as.data.frame(mvrnorm(n = N, Sigma=sigma, mu=c(0,0)))
+      colnames(a)  <- c("x","z")
+      a$xz <- a$x*a$z
+
+      error <-  rnorm(N,0,sqrt(e))   # the amount of error determines the ES
+
+      a$y <- sqrt(b1)*a$x + sqrt(b2)*a$z + sqrt(b3)*a$xz + error
+
+      res <- lm( y ~ x + z + xz, data=a) 
+      res <- lm.beta(res)
+      sig <- summary(res)$coefficients[4,5] < .05
+      rsq <- summary(res)$r.squared
+ 
+      out[i,] <- c(rsq,  res$standardized.coefficients[-1], sig)
+  
+            }   # loop over replications
+           
+           
+           result[((result$e == e) & (result$rho == rho) & result$N == N),c(4:8)] <- apply(out,2,mean)
+           print(c(N, e,rho))
+           
+          }     # loop over correlations between predictors
+         }      # loop over error levels
+       }        # loop over sample sizes
+    
+         return(result)
+
+} # end function
 
 
-apply(out,2,mean)
-mean(varxz)
-mean(vary)
+
+res <- simPower.moderation(samSize = c(100,150,200,250,300),  errlevel = c(1,3,9), rholevel = c(0.3), betas = c(.5,.3,.2), rep=1000) 
+
+
+res[(res$rho == 0.3),]
+
+  
+save(res, file= "result_moderatie.Rdata")
+  
+require(pander)
+pander(res) 
+
+require(ggplot2)
+
+
+p <- ggplot(data=res, aes(y=power, x=N, group=e, colour=e)) + geom_point() + geom_line()
+p <- p + geom_hline(yintercept=0.80, linetype="dashed", color = "red")
+
+
+  
+p
+  
+   ## extra tests
+  
+  
+
 
 # R squared
 
-(mean(vary) - e)/(mean(vary) )
+(mean(vary) - mean(err))/(mean(vary) )
 
 
 ### ES as function of variance of error

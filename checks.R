3964fc43af07d1867886c6a66d2e76c5a904840d
PeterVerboon <Peter.Verboon@ou.nl>
2018-08-07 16:56:58 +0200

    function checkPars

--- checks.R
+++ checks.R
@@ -1,51 +1,72 @@
 
-ve <- 9    # 1, 3, 9
-r <- .3    # 0, 0.3, 0.5, 0.8
-ar <- .8    # 0, 0.2, 0.8
 
-b1 <- .5
-b2 <- .3
-b3 <- .2
-b4 <- ar
+# DEFINE FUNCTION
+# Assume x and z ~ N(0,1)
+# check expected effect sizes using formulas from power moderation paper
 
-vy1 <- (b1) + (b2) + (1+r**2)*(b3) + 2*sqrt(b1)*sqrt(b2)*r                                # with interaction, no AR
-vy <- (b1) + (b2) + (1+r**2)*(b3) + 2*sqrt(b1)*sqrt(b2)*r + (ar)*sqrt(vy1)                # with interaction, with AR
-vy <- (b1) + (b2) + (1+r**2)*(b3) + 2*sqrt(b1)*sqrt(b2)*r + (ar)*sqrt(vy1) + (ve)      # with interaction, with AR, with error
+ checkPars  <- function(rholevel = c(0.0, 0.3, 0.8),
+                  arlevel = c(0,0.8),
+                  errlevel = c(0,1,3,9),
+                  bpar = c(.5, .3, .2))
+   {
+   b1 <- bpar[1]
+   b2 <- bpar[2]
+   b3 <- bpar[3]
+ 
+   numrow <- length(rholevel)*length(arlevel)*length(errlevel)
+   result <- as.data.frame(matrix(data=0, nrow = numrow, ncol= 8))
+   colnames(result) <- c("AR","error","rho","b1","b2","b3","b4","rsq")
+   result[,"error"] <- rep(sort(rep(errlevel,length(rholevel))),length(arlevel))
+   result[,"rho"] <- rep(rholevel,length(errlevel)*length(arlevel))
+   result[,"AR"] <- sort(rep(arlevel,length(errlevel)*length(rholevel)))
+   
+
+     for (e in errlevel) 
+     { 
+       for (r in rholevel)
+       {
+         for (ar in arlevel) 
+         {
+           
+   
+   vy0 <- (b1) + (b2) + (1+r**2)*(b3) + 2*sqrt(b1)*sqrt(b2)*r             # with interaction, no AR
+   vy2 <- vy0 + (ar)*vy0 + e                               # with interaction, with AR, with error
+   
+   be1 <- sqrt(b1)/sqrt(vy2)
+   be2 <- sqrt(b2)/sqrt(vy2)
+   be3 <- sqrt(b3)*(sqrt(1+r**2)/sqrt(vy2))
+   be4 <- sqrt(ar)/sqrt(vy2)
+
+   rsq <- (vy2 - e)/vy2   # R squared
+   
+   result[((result$error == e) & (result$rho == r) & result$AR == ar),c(4:8)] <- c(be1,be2,be3,be4,rsq)
+   
+         } # end ar level
+       } # end rho level
+     } # end error level
+  
+  return(result)
+   
+ } # end function
 
 
-be1 <- (b1)/sqrt(vy)
-be2 <- (b2)/sqrt(vy)
-be3 <- (b3)*(sqrt(1+r**2)/sqrt(vy))
-be4 <- (b4)*(sqrt(vy1)/sqrt(vy))
+ 
+ # test
+ 
+out <- checkPars(rholevel = c(0.0, 0.3, 0.6),
+                 arlevel = c(0.0,0.5),
+                 errlevel = c(0,1,3,9),
+                 bpar = c(.5, .3, .2))
 
 
-# R squared
 
-(vy - ve)/vy
-
-(mean(vary) - mean(err))/(mean(vary) )
-
-
-### ES as function of variance of error
-
-sqrt(.5)/sqrt(2.5)
-sqrt(.3)/sqrt(2.5)
-sqrt(.2)/sqrt(2.5)
-
-## with correlated predictors
-
-sqrt(.5) * (1 / sqrt(mean(vary)))
-sqrt(.3) * (1 / sqrt(mean(vary)))
-sqrt(.2) * (sqrt(mean(varxz)) / sqrt(mean(vary)))
 
 
 
 ## expected variance of product xz
 
 1 + cov((x**2),(z**2)) - (cov(x,z)**2)
-
 1 + (2*rho1**2) - (cov(x,z)**2)
 
 
-str(res)
 

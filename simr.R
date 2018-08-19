1631782ecb392382c40610f0ce669e59e74d7cd3
Verboon <pve@ou.nl>
2018-08-19 11:05:38 +0200

    text

--- simr.R
+++ simr.R
@@ -8,7 +8,7 @@ X <- expand.grid(x=x, g=g)
 b <- c(2, -0.1) # fixed intercept and slope
 V1 <- 1.0 # random intercept variance
 V2 <- matrix(c(0.5,0.05,0.05,0.1), 2) # random intercept and slope variance-covariance matrix
-s <- 1 # residual variance
+s <- 10 # residual variance
 
 model1 <- makeLmer(y ~ x + (1|g), fixef=b, VarCorr=V1, sigma=s, data=X)
 
@@ -16,7 +16,7 @@ model1 <- makeLmer(y ~ x + (1|g), fixef=b, VarCorr=V1, sigma=s, data=X)
 
 print(model1)
 
-powerSim(model1, nsim=20)
+a <- powerSim(model1, nsim=100)
 
 ##############
 
@@ -35,3 +35,6 @@ plot(pc2)
 
 save(pc2, file= "result_MLAmod_alpha05.Rdata")
 #load("result_moderation_alpha05.Rdata")
+
+binom::binom.confint(15, n=100)
+
